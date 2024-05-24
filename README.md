f34ae140533a76cad22f4040f296aa355aa0c516
higger <matt.higger@gmail.com>
2024-05-24 11:47:03 -0400

    add - requery & exponetial delay (per openai api doc)
    unfinished - aynch call (issues with child process outliving parent, I think).  I'm deprecating with next commit

--- README.md
+++ README.md
@@ -14,4 +14,4 @@ to generate [2s_comp_overflow00.tex](/example/2s_comp_overflow01.tex)
 
 # Account Setup
 
-You'll need to sign up for openai's API service (min sign up is $5 in May 2024, to create the problem above cost < $0.01). Once you've got an account, [make an API key](https://platform.openai.com/api-keys) and store it in [key.py](/gpt_dup/key.py).  (Alternatively, if no key is found, the software will ask with each run).
+You'll need to sign up for openai's API service (min sign up is $5 in May 2024, to create the problem above cost < $0.01). Once you've got an account, [make an API key](https://platform.openai.com/api-keys) and store it in [key.py](/gpt_dup/key.py).
--- gpt_dup/__main__.py
+++ gpt_dup/__main__.py
@@ -1,11 +1,27 @@
 import argparse
+import os
 import pathlib
+import platform
 import re
+import sys
+from datetime import datetime
+from subprocess import Popen, PIPE
 
 from openai import OpenAI
 
+from gpt_dup.key import api_key
 
-def main():
+client = OpenAI(api_key=api_key)
+
+from tenacity import retry, stop_after_attempt, wait_random_exponential
+
+
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+def completion_with_backoff(**kwargs):
+    return client.chat.completions.create(**kwargs)
+
+
+def duplicate_and_save_cli():
     description = """ 
     Duplicates and refreshes a given math problem via chatGPT, updating 
     numbers and solutions as needed.  Results will vary, but this tool has been
@@ -15,51 +31,76 @@ def main():
     parser = argparse.ArgumentParser(description=description)
 
     parser.add_argument('-f', '--file', type=str, help='Input File',
-                        required=True)
-    parser.add_argument('-k', '--key', type=str, help='openai api key')
+                        required=True, dest='file')
+    parser.add_argument('-o', '--out', type=str, dest='file_out',
+                        help='Output File (will add index to input by default)')
     parser.add_argument('-q', '--quiet', action='store_true',
-                        help='run in quiet mode')
+                        help='run in quiet mode', dest='quiet')
+    parser.add_argument('-a', '--async', action='store_true',
+                        help='run asynchronously (new process)', dest='asynch')
     args = parser.parse_args()
 
-    new_prob = duplicate(file=args.file)
-    file_out = get_file_out_add_one(file=args.file)
+    duplicate_and_save(file=args.file, file_out=args.file_out,
+                       verbose=not args.quiet, asynch=args.asynch)
+
+
+def duplicate_and_save(file, file_out=None, verbose=True, asynch=False):
+    # choose a file out, write a placeholder file with timestamp
+    if file_out is None:
+        file_out = get_file_out_add_one(file=file)
+
+    if verbose:
+        print(f'new problem requested: {file_out}')
+
+    if asynch:
+        # asynchronous
+        cmd = [sys.executable, __file__, '-f', file, '-o', file_out]
+        if not verbose:
+            cmd += ['-q']
+
+        # ensure process outlives current one (waits to receive API
+        # response)
+        if platform.system() in ["Linux", "Darwin"]:
+            kwargs = dict(preexec_fn=os.setsid)
+        elif platform.system() == "Windows":
+            NotImplementedError('Async mode doesnt work in Windows')
+        else:
+            RuntimeError(f'OS not recognized: {platform.system()}')
+
+        proc = Popen(cmd, stdout=PIPE, stderr=PIPE, **kwargs)
+
+        return file_out
+
+    new_prob = duplicate(file=file)
 
     with open(file_out, 'w') as f:
         f.write(new_prob)
 
-    if not args.quiet:
+    if verbose:
         print(new_prob)
         print(f'written to: {file_out}')
 
+    return file_out
 
-def duplicate(file=None, s=None, model='gpt-4o', api_key=None):
+
+def duplicate(file=None, s=None, model='gpt-4o'):
     assert (file is None) != (s is None), 'file xor s required'
 
     if s is None:
         s = open(file).read()
 
-    if api_key is None:
-        from gpt_dup.key import api_key
-    if api_key is None:
-        api_key = input('No OPENAI_API_KEY environment variable found, '
-                        'enter here:')
-
-    client = OpenAI(api_key=api_key)
-
-    completion = client.chat.completions.create(
-        model=model,
-        messages=[
-            {'role': 'user',
-             'content': f'Provide a file in a similar format, rewriting '
-                        f'the math problem with new numbers and updating '
-                        f'the solutions as needed to ensure correctness?\n'
-                        f'"{s}"'}
-        ]
-    )
+    prompt = '''Provide a file in a similar format, rewriting the math problem 
+    with new numbers and updating the solutions as needed to ensure 
+    correctness?  Your response should include only the latex, do not include 
+    anything in your response which is not latex.
+    '''
+    messages = [{'role': 'user',
+                 'content': prompt + s}]
+    completion = completion_with_backoff(model=model, messages=messages)
     return completion.choices[0].message.content
 
 
-def get_file_out_add_one(file, z_fill_width=2):
+def get_file_out_add_one(file, z_fill_width=2, touch=True):
     file = pathlib.Path(file)
 
     # get old idx (if present)
@@ -75,10 +116,16 @@ def get_file_out_add_one(file, z_fill_width=2):
     for _ in range(1000):
         file_out = file.with_stem(stem_old + str(new_idx).zfill(z_fill_width))
         if not file_out.exists():
+            if touch:
+                with open(file_out, 'w') as f:
+                    s_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    print(f'requested at: {s_time} (this will be overwritten)',
+                          file=f)
             return file_out
+        new_idx += 1
 
     raise RuntimeError(f'no unique file out created: {file}')
 
 
 if __name__ == '__main__':
-    main()
+    duplicate_and_save_cli()
